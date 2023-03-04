b366f710fe75d8e2c27f2005e2ed95d4f8fc0492
Fernando <fernando.t10@outlook.com>
2023-03-04 00:08:29 -0500

    Readme created w/ videos

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
       return;
     }
     
-    res.status(200).json(categoruData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
